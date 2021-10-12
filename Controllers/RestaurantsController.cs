830fc6be60006dacb7341587b913bbaec5959638
Mariusz Kiesiński <mkiesinski@gmail.com>
2021-10-12 20:49:37 +0200

    Add test restaurant service inbound controller

---
+++ Controllers/RestaurantsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Controllers
+{
+    [Route("api/v1/u/[controller]")]
+    [ApiController]
+    public class RestaurantsController : ControllerBase
+    {
+        public RestaurantsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # User Service");
+
+            return Ok("Inbound test Restaurant Controller");
+        }
+    }
+}
